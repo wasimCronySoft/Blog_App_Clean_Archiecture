71b149e4334599dca657b4383a1133db4c1e4976
Netesh5 <netesh.paudel@gmail.com>
2024-03-15 22:59:43 +0545

    feat: added signupInteractor

---
+++ lib/features/auth/domain/usecases/signup_interactor.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:clean_arch_application/core/error/exception.dart';
+import 'package:dartz/dartz.dart';
+
+import 'package:clean_arch_application/core/error/failure.dart';
+import 'package:clean_arch_application/core/usecases/async_usecase.dart';
+import 'package:clean_arch_application/features/auth/domain/entities/signup_entity.dart';
+import 'package:clean_arch_application/features/auth/domain/repositories/auth_repository.dart';
+
+class SignUpInteractor implements AsyncUseCase<String, SignUpEntity> {
+  AuthRepository repository;
+  SignUpInteractor({
+    required this.repository,
+  });
+
+  @override
+  Future<Either<Failure, String>> call(param) async {
+    try {
+      final res = await repository.signupwithEmailPassword();
+      return Right(res);
+    } on CustomException catch (e) {
+      return Left(Failure(e.message));
+    } catch (e) {
+      return Left(Failure(e.toString()));
+    }
+  }
+}
