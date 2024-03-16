6c1868903b37bce112af7c42c3efa39f73b6be49
Netesh5 <netesh.paudel@gmail.com>
2024-03-16 23:56:57 +0545

    feat: refactored the  user state to check login

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:clean_arch_application/core/Injector/injector.dart';
+import 'package:clean_arch_application/core/cubits/app_user/app_user_cubit.dart';
 
 import 'package:clean_arch_application/core/routes/routes.dart';
 import 'package:clean_arch_application/core/routes/routes_constants.dart';
 import 'package:clean_arch_application/core/theme/app_theme.dart';
+import 'package:clean_arch_application/features/auth/domain/usecases/login_interactor.dart';
+import 'package:clean_arch_application/features/auth/domain/usecases/signup_interactor.dart';
+import 'package:clean_arch_application/features/auth/presentation/bloc/auth_bloc.dart';
 
 import 'package:clean_arch_application/wrapper.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,12 +31,27 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.ligthTheme,
-      onGenerateRoute: RoutesGenerator.generateRoutes,
-      initialRoute: Routes.login,
-      home: const Wrapper(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AppUserCubit(),
+        ),
+        BlocProvider(
+          create: (context) => AuthBloc(
+            singupInteractor: DI.instance<SignUpInteractor>(),
+            loginInteractor: DI.instance<LoginInteractor>(),
+            userInteractor: DI.instance(),
+            appUserCubit: DI.instance(),
+          ),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.ligthTheme,
+        onGenerateRoute: RoutesGenerator.generateRoutes,
+        initialRoute: Routes.login,
+        home: const Wrapper(),
+      ),
     );
   }
 }
