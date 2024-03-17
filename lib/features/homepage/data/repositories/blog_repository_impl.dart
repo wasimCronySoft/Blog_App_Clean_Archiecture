f1677b6ef436d72fea0322e10ca6328bd60d04c6
Netesh5 <netesh.paudel@gmail.com>
2024-03-17 21:20:01 +0545

    feat: added function to retrive blog offline

--- lib/features/homepage/data/repositories/blog_repository_impl.dart
+++ lib/features/homepage/data/repositories/blog_repository_impl.dart
@@ -1,21 +1,27 @@
 import 'dart:io';
 
+import 'package:clean_arch_application/core/network/internet_checker.dart';
+import 'package:clean_arch_application/features/homepage/data/datasources/blog_local_data_source.dart';
 import 'package:clean_arch_application/features/homepage/data/datasources/blog_remote_data_source.dart';
 import 'package:clean_arch_application/features/homepage/domain/entities/blog.dart';
 import 'package:clean_arch_application/features/homepage/domain/entities/blog_params.dart';
 import 'package:clean_arch_application/features/homepage/domain/repositories/blog_repository.dart';
 
 class BlogRepositoryImpl implements BlogRepository {
-  final BlogRemoteDatatSource dataSource;
+  final BlogRemoteDatatSource remoteDatatSource;
+  final BlogLocalDataSource blogLocalDataSource;
+  final ConnectionChecker connectionChecker;
   BlogRepositoryImpl({
-    required this.dataSource,
+    required this.remoteDatatSource,
+    required this.blogLocalDataSource,
+    required this.connectionChecker,
   });
   @override
   Future<void> uploadBlog(
       {required BlogParams params, required File file}) async {
     final imageString =
-        await dataSource.uploadBlogImage(file: file, params: params);
-    final res = await dataSource.uploadBlog(
+        await remoteDatatSource.uploadBlogImage(file: file, params: params);
+    final res = await remoteDatatSource.uploadBlog(
       params: params.copyWith(
         imageUrl: imageString,
       ),
@@ -25,7 +31,12 @@ class BlogRepositoryImpl implements BlogRepository {
 
   @override
   Future<List<Blog>> fetchBlogs() async {
-    final res = await dataSource.fetchBlog();
-    return res;
+    if (!(await connectionChecker.isConnected)) {
+      final res = blogLocalDataSource.loadBlogs();
+      return res;
+    } else {
+      final res = await remoteDatatSource.fetchBlog();
+      return res;
+    }
   }
 }
